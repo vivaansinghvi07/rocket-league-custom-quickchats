b71ed5ce252f7314aeac42df49b70f8bd54af2de
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-21 01:25:42 -0400

    added quickchats program

---
+++ prompts.py
@@ -0,0 +1,18 @@
+### EDIT QUICKCHATS HERE
+QUICKCHATS = [["Quickchat Placeholder 1", 
+               "Quickchat Placeholder 2",
+               "Quickchat Placeholder 3",
+               "Quickchat Placeholder 4"],
+              ["Quickchat Placeholder 5",
+               "Quickchat Placeholder 6",
+               "Quickchat Placeholder 7",
+               "Quickchat Placeholder 8"],
+              ["Quickchat Placeholder 9",
+               "Quickchat Placeholder 10",
+               "Quickchat Placeholder 11",
+               "Quickchat Placeholder 12"],
+              ["Quickchat Placeholder 13",
+               "Quickchat Placeholder 14",
+               "Quickchat Placeholder 15",
+               "Quickchat Placeholder 16"]]
+
