202f69020eecf5d98791d90eba6cf347a489c942
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-21 10:41:08 -0400

    added combination comments

--- prompts.py
+++ prompts.py
@@ -1,19 +1,18 @@
 ### EDIT QUICKCHATS HERE
-class Chat():
-    quickchats = [["Quickchat Placeholder 1", 
-                   "Quickchat Placeholder 2",
-                   "Quickchat Placeholder 3",
-                   "Quickchat Placeholder 4"],
-                  ["Quickchat Placeholder 5",
-                   "Quickchat Placeholder 6",
-                   "Quickchat Placeholder 7",
-                   "Quickchat Placeholder 8"],
-                  ["Quickchat Placeholder 9",
-                   "Quickchat Placeholder 10",
-                   "Quickchat Placeholder 11",
-                   "Quickchat Placeholder 12"],
-                  ["Quickchat Placeholder 13",
-                   "Quickchat Placeholder 14",
-                   "Quickchat Placeholder 15",
-                   "Quickchat Placeholder 16"]]
-
+class Chat():                                       # THE COMBINATIONS FOR THE CHATS ARE SHOWN HERE
+    quickchats = [["Quickchat Placeholder 1",       # up - up 
+                   "Quickchat Placeholder 2",       # up - left
+                   "Quickchat Placeholder 3",       # up - down
+                   "Quickchat Placeholder 4"],      # up - right  
+                  ["Quickchat Placeholder 5",       # left - up
+                   "Quickchat Placeholder 6",       # left - left
+                   "Quickchat Placeholder 7",       # left - down
+                   "Quickchat Placeholder 8"],      # left - right
+                  ["Quickchat Placeholder 9",       # down - up
+                   "Quickchat Placeholder 10",      # down - left
+                   "Quickchat Placeholder 11",      # down - down 
+                   "Quickchat Placeholder 12"],     # down - right
+                  ["Quickchat Placeholder 13",      # right - up
+                   "Quickchat Placeholder 14",      # right - left
+                   "Quickchat Placeholder 15",      # right - down
+                   "Quickchat Placeholder 16"]]     # right - right
