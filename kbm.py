ed1a2b9244e85515fdfdbaedc58976738203cfba
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-21 13:32:59 -0400

    added comments and removed unneeded imports

--- kbm.py
+++ kbm.py
@@ -1,6 +1,6 @@
-from pynput.keyboard import Events, Controller, Listener, Key        # imports keyboard listening and the keyboard controller
+from pynput.keyboard import Controller, Listener       # imports keyboard listening and the keyboard controller
 from printer import getPrompt, printPrompt          # gets quickchat printing methods
-from settings import Chat 
+from settings import Chat
 
 class KbmQuickchat():
 
@@ -9,15 +9,22 @@ class KbmQuickchat():
     CHATBIND = Chat.chatbind            # the chatbind (usually t)
 
     def __init__(self):
+
+        # starter values
         self.store = -1
         self.waitForAnother = False
+
+        # gets the keyboard for output
         self.keyboard = Controller()
 
     def on_press(self, key):
-        print(key)
         try:
             if key.char in ['1', '2', '3', '4']:       # only detects for 1, 2, 3, 4
+
+                # toggles storage
                 self.waitForAnother = not self.waitForAnother
+
+                # if we need to store a number, do it
                 if self.waitForAnother:
                     self.store = int(key.char)
                 else:
@@ -26,8 +33,9 @@ class KbmQuickchat():
             return
 
     def run(self):
-        with Listener(on_press=self.on_press) as listener:
+        with Listener(on_press=self.on_press) as listener:      # listens to every key press
             listener.join()
     
+# runs the thing
 chat = KbmQuickchat()
 chat.run()
