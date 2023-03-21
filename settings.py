35dc94d155868d80a5a62fff934799dd2ec6b9e9
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-21 12:44:42 -0400

    removed numbers on placeholders

--- settings.py
+++ settings.py
@@ -1,21 +1,21 @@
 class Chat():
     ### EDIT QUICKCHATS HERE                        # THE COMBINATIONS FOR THE CHATS ARE SHOWN HERE
-    quickchats = [["Quickchat Placeholder 1",       # up - up 
-                   "Quickchat Placeholder 2",       # up - left
-                   "Quickchat Placeholder 3",       # up - down
-                   "Quickchat Placeholder 4"],      # up - right  
-                  ["Quickchat Placeholder 5",       # left - up
-                   "Quickchat Placeholder 6",       # left - left
-                   "Quickchat Placeholder 7",       # left - down
-                   "Quickchat Placeholder 8"],      # left - right
-                  ["Quickchat Placeholder 9",       # down - up
-                   "Quickchat Placeholder 10",      # down - left
-                   "Quickchat Placeholder 11",      # down - down 
-                   "Quickchat Placeholder 12"],     # down - right
-                  ["Quickchat Placeholder 13",      # right - up
-                   "Quickchat Placeholder 14",      # right - left
-                   "Quickchat Placeholder 15",      # right - down
-                   "Quickchat Placeholder 16"]]     # right - right
+    quickchats = [["Quickchat Placeholder",         # up - up 
+                   "Quickchat Placeholder",         # up - left
+                   "Quickchat Placeholder",         # up - down
+                   "Quickchat Placeholder"],        # up - right  
+                  ["Quickchat Placeholder",         # left - up
+                   "Quickchat Placeholder",         # left - left
+                   "Quickchat Placeholder",         # left - down
+                   "Quickchat Placeholder"],        # left - right
+                  ["Quickchat Placeholder",         # down - up
+                   "Quickchat Placeholder",         # down - left
+                   "Quickchat Placeholder",         # down - down 
+                   "Quickchat Placeholder"],        # down - right
+                  ["Quickchat Placeholder",         # right - up
+                   "Quickchat Placeholder",         # right - left
+                   "Quickchat Placeholder",         # right - down
+                   "Quickchat Placeholder"]]        # right - right
     
     # EDIT KEYBOARD CHAT BIND HERE
     chatbind = 't'
