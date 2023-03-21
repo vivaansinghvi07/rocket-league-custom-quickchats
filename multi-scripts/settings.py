4c07dfa6646d8be8fe482ce8211d8ba6bee472df
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-03-21 18:33:59 -0400

    reverted to placeholders

--- multi-scripts/settings.py
+++ multi-scripts/settings.py
@@ -1,21 +1,21 @@
 class Chat():
     ### EDIT QUICKCHATS HERE                        # THE COMBINATIONS FOR THE CHATS ARE SHOWN HERE: BOTH KBM AND CONTROLLER
-    quickchats = [["let me cook",         # controller: up - up           kbm: 1 - 1
-                   "i'm the best",         # controller: up - left         kbm: 1 - 2
-                   "you guys suck!",         # controller: up - down         kbm: 1 - 3
-                   "hit the ball!"],        # controller: up - right        kbm: 1 - 4
-                  ["amazing shot!!!!",         # controller: left - up         kbm: 2 - 1
-                   "nice pass",         # controller: left - left       kbm: 2 - 2
-                   "WHAT A SAVE!!",         # controller: left - down       kbm: 2 - 3
-                   "thank you so much"],        # controller: left - right      kbm: 2 - 4
-                  ["i meant to do that",         # controller: down - up         kbm: 3 - 1
-                   "no problemo",         # controller: down - left       kbm: 3 - 2
-                   "sorry my bad",         # controller: down - down       kbm: 3 - 3
-                   "whoopsies!"],        # controller: down - right      kbm: 3 - 4
-                  ["NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",         # controller: right - up        kbm: 4 - 1
-                   "this is rokt leeg!",         # controller: right - left      kbm: 4 - 2
-                   "demoing",         # controller: right - down      kbm: 4 - 3
-                   "passing"]]        # controller: right - right     kbm: 4 - 4
+    quickchats = [["Quickchat Placeholder",         # controller: up - up           kbm: 1 - 1
+                   "Quickchat Placeholder",         # controller: up - left         kbm: 1 - 2
+                   "Quickchat Placeholder",         # controller: up - down         kbm: 1 - 3
+                   "Quickchat Placeholder"],        # controller: up - right        kbm: 1 - 4
+                  ["Quickchat Placeholder",         # controller: left - up         kbm: 2 - 1
+                   "Quickchat Placeholder",         # controller: left - left       kbm: 2 - 2
+                   "Quickchat Placeholder",         # controller: left - down       kbm: 2 - 3
+                   "Quickchat Placeholder"],        # controller: left - right      kbm: 2 - 4
+                  ["Quickchat Placeholder",         # controller: down - up         kbm: 3 - 1
+                   "Quickchat Placeholder",         # controller: down - left       kbm: 3 - 2
+                   "Quickchat Placeholder",         # controller: down - down       kbm: 3 - 3
+                   "Quickchat Placeholder"],        # controller: down - right      kbm: 3 - 4
+                  ["Quickchat Placeholder",         # controller: right - up        kbm: 4 - 1
+                   "Quickchat Placeholder",         # controller: right - left      kbm: 4 - 2
+                   "Quickchat Placeholder",         # controller: right - down      kbm: 4 - 3
+                   "Quickchat Placeholder"]]        # controller: right - right     kbm: 4 - 4
     
     # EDIT KEYBOARD CHAT BIND HERE
     chatbind = 't'
