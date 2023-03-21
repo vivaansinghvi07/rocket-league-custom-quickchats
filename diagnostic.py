54a56e3f2c3b43601f66c891f6b0b83bf58812b0
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-03-21 19:07:32 -0400

    upgraded diagnostic test

--- diagnostic.py
+++ diagnostic.py
@@ -1,9 +1,23 @@
+# allows for library download outside of OS
+import sys
+import subprocess
+
+# download libaries needed
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'inputs'])
+
 from inputs import get_gamepad                  # for detecting controller on your computer
 
-# INSTRUCTIONS: Run this, with your controller connected,
-# and press your dpad buttons, and the program will spit our what they are called.
-# When the program outputs their codes, enter them in main.py under the X and Y constants
+"""
+INSTRUCTIONS: Run this, with your controller connected, and
+press your dpad buttons, and the program will spit out what
+what they are called. When the program outputs their codes,
+use them as directed by the README file on GitHub. Keep the 
+fact that there will be only two different codes: an X (for 
+left-right) and a Y (for up-down) in mind. To terminate this 
+process, close theTerminal window or press Ctrl+C.
+""" 
 
+# runs diagnostic test
 def test():                                     
     while True:                                 
         events = get_gamepad()
