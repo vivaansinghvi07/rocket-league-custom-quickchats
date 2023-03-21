6934f0cd93d012aeeff7dbe7de886caafbe9054f
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-21 01:39:19 -0400

    comments and QOL changes

--- main.py
+++ main.py
@@ -1,52 +1,71 @@
-from inputs import get_gamepad
-from pynput.keyboard import Key, Controller
+from inputs import get_gamepad                  # for detecting controller on your computer
+from pynput.keyboard import Key, Controller     # for sending text
+from prompts import Chat
 import time
 
-def getPrompt(store, number):
-    prompts = [["Amazing shot!!",
-               "I'm the greatest!",
-               "let me cook",
-               "go for the fricking ball"],
-               ["sorry my bad whoopsies",
-               "no problemo",
-               "outta boost bro",
-               "my fault dawg"],
-               ["what a save",
-               "quickchat placeholder 1",
-               "quickchat placeholder 2",
-               "vroom vroom"],
-               ["car ball",
-               "this is rokt leeg",
-               "hi",
-               "good game"]]
-    
-    return prompts[store-1][number-1]
+DPADX = "ABS_HAT0X"                 # codes for d-pads
+DPADY = "ABS_HAT0Y"
+CHATBIND = "t"                      # your rocket league chat binding
+QUICKCHATS = Chat.quickchats        # the quickchats used
+
+def getPrompt(store, number, quickchats):
+    return quickchats[store-1][number-1]        # returns the corresponding quickchat
 
 def printPrompt(keyboard, prompt):
-    keyboard.press('t')
-    keyboard.release('t')
+
+    # presses the chat binding
+    keyboard.press(CHATBIND)
+    keyboard.release(CHATBIND)
+
+    # waits a little to make it work
     time.sleep(0.01)
+
+    # presses every letter
     for letter in prompt:
-        keyboard.press(letter)
-        keyboard.release(letter)
+        if letter.isupper():
+            with keyboard.press(Key.shift):             # if the letter is uppercase, hold shift while doing it
+                keyboard.press(letter.lower())
+                keyboard.release(letter.lower())
+        else:                                           # otherwise print as normal
+            keyboard.press(letter)
+            keyboard.release(letter)
+            
+    # sends chat
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
 
 def main():
+
+    # stores values
     waitForAnother = False
     store = -1
+
+    # creates a keybaord object
     keyboard = Controller()
-    while 1:
+    while True:
+
+        # gets the gamepad plugged in
         events = get_gamepad()
+
+        # goes for each event
         for event in events:
-            if (event.code.strip() == "ABS_HAT0X" or event.code.strip() == "ABS_HAT0Y") and event.state != 0:
+
+            # if its a dpad event (for quickchats)
+            if (event.code.strip() == DPADX or event.code.strip() == DPADY) and event.state != 0:
+
+                # makes it so that you need to click two dpads to send a chat
                 waitForAnother = not waitForAnother
-                code = 1 if event.code.strip() == "ABS_HAT0X" else 0
-                number = event.state + 2 + code         # creates nums from 1, 2, 3, 4
-                if waitForAnother:
+
+                # gets the code (for making chats unique)
+                code = 1 if event.code.strip() == DPADX else 0
+                
+                # creates nums from 1, 2, 3, 4 (state can be -1 a or 1)
+                number = event.state + 2 + code         
+
+                if waitForAnother:          # if we need to wait for another update the stored value to the number
                     store = number
-                else:
-                    printPrompt(keyboard, getPrompt(store, number))
+                else:                       # otherwise, print the chat
+                    printPrompt(keyboard, getPrompt(store, number, QUICKCHATS))
 
 
 if __name__ == "__main__":
